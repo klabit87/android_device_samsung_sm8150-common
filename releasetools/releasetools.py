f618d2e3b213466323d0e54aeffdb53bc097168a
klabit87 <keith.labit@gmail.com>
2020-09-29 20:04:29 -0500

    Remove for now

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -26,6 +26,10 @@ def IncrementalOTA_Assertions(info):
   return
 
 def FullOTA_InstallEnd(info):
+  info.script.AppendExtra('run_program("/sbin/mount", "-t", "auto", "/system_root");');
+  info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");');
+  info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");');
+  info.script.AppendExtra('run_program("/sbin/umount", "-t", "auto", "/system_root");');
   OTA_InstallEnd(info)
   return
 
